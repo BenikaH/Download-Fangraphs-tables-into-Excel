b7175a44b891a57ed03af33cad129429162f4283
miketibb <mike_tibb@yahoo.com>
2016-03-21 22:23:47 -0500

    Update fangraphs.py

--- fangraphs.py
+++ fangraphs.py
@@ -18,22 +18,22 @@ def fangraphs(name):
             if page == []:
                 print('Nothing here.')
             else:
-                for h in range(1, len(page)):
+                for h in range(len(page)):
                     stats.setdefault(page[h].getText(), [])
                     stats2.append(page[h].getText())
             page2 = soup.find_all('td', {'class':['grid_line_regular','grid_line_break']})
             c = 0
             for d in range(len(page2)):
                 #print(page2[d].getText())
-                if page2[d].getText() == '':
-                    stats[stats2[c]].append('NA')
+                if page2[d].getText() == '\xa0':
+                    stats[stats2[c]].append(' ')
                 else:
                     stats[stats2[c]].append(page2[d].getText())
                 c += 1
-                if c == 21:    
+                if c == 21:
                     c = 0
-            print(stats[stats2[0]])
-            print(stats2[0])
+            #print(stats[stats2[0]])
+            #print(stats2[0])
 fangraphs('Mike Trout')          
 
                    
